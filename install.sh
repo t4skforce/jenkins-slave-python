068728492d4f80df20e6f5a87cd03100e1659551
t4skforce <7422037+t4skforce@users.noreply.github.com>
2018-07-20 01:05:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
+
+symlink () {
+  [ -e $2 ] && rm $2
+  [ -e $1 ] && ln -s $1 $2
+}
+
 for VERSION in $PYTHON_VERSIONS;do
   MAJOR_VERSION="${VERSION::3}"
   echo $VERSION $MAJOR_VERSION
@@ -10,8 +16,13 @@ for VERSION in $PYTHON_VERSIONS;do
   mkdir -p /opt/python-${MAJOR_VERSION}
   ./configure --prefix=/opt/python-${MAJOR_VERSION} --cache-file=/opt/python-${MAJOR_VERSION}/config.cache --enable-optimizations > /dev/null
   make -s -j8
-  make install
+  make altinstall
   ls -la /opt/python-${MAJOR_VERSION}/bin/
-  ln -s /opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION} /usr/local/bin/python${MAJOR_VERSION}
-  ln -s /opt/python-${MAJOR_VERSION}/bin/pip${MAJOR_VERSION} /usr/local/bin/pip${MAJOR_VERSION}
+  
+  symlink "/usr/local/bin/python${MAJOR_VERSION}" "/usr/local/bin/python${MAJOR_VERSION}"
+  symlink "/usr/local/bin/pip${MAJOR_VERSION}" "/usr/local/bin/pip${MAJOR_VERSION}"
+  symlink "/usr/local/bin/pyvenv-${MAJOR_VERSION}" "/usr/local/bin/pyvenv-${MAJOR_VERSION}"
+  symlink "/usr/local/bin/easy_install-${MAJOR_VERSION}" "/usr/local/bin/easy_install-${MAJOR_VERSION}"
+  symlink "/usr/local/bin/idle-${MAJOR_VERSION}" "/usr/local/bin/idle-${MAJOR_VERSION}"
+
 done
