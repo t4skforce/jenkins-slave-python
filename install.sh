9fd6c6bf502ae599b12621646eb8865346054a98
t4skforce <7422037+t4skforce@users.noreply.github.com>
2018-07-19 21:18:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ for VERSION in $PYTHON_VERSIONS;do
   cd /tmp/Python-${VERSION}
   mkdir -p /opt/python-${MAJOR_VERSION}
   ./configure --prefix=/opt/python-${MAJOR_VERSION} --enable-optimizations > /dev/null
+  make > /dev/null
+  make install
   ls -la /opt/python-${MAJOR_VERSION}/bin/
   ln -s /opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION} /usr/local/bin/python${MAJOR_VERSION}
   ln -s /opt/python-${MAJOR_VERSION}/bin/pip${MAJOR_VERSION} /usr/local/bin/pip${MAJOR_VERSION}
