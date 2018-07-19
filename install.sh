bf9b2aabe0e456d8120e821e45105a6abd5159fc
t4skforce <7422037+t4skforce@users.noreply.github.com>
2018-07-19 20:19:44 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+for VERSION in $PYTHON_VERSIONS;do
+  MAJOR_VERSION="${VERSION::3}"
+  echo $VERSION $MAJOR_VERSION
+  cd /tmp/
+  curl -Ln https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output Python-${VERSION}.tgz
+  tar zxf Python-${VERSION}.tgz
+  cd Python-${PYTHON27}
+  mkdir -p /opt/python-${MAJOR_VERSION}
+  ./configure --prefix=/opt/python-${MAJOR_VERSION}
+  ln -s /opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION} /usr/local/bin/python${MAJOR_VERSION}
+  ln -s /opt/python-${MAJOR_VERSION}/bin/pip${MAJOR_VERSION} /usr/local/bin/pip${MAJOR_VERSION}
+done
