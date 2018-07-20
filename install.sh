1d24889cf1deac8df62e7bd6044baa81f34e7fc3
t4skforce <7422037+t4skforce@users.noreply.github.com>
2018-07-20 08:22:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 set -e
 
 create_symlink () {
-  if [ -e "$2" ]; then
-    rm "$2"
-  fi
-  if [ -e "$1" ]; then
-    ln -s "$1" "$2"
+  if [ ! -e "$2" ]; then
+    if [ -e "$1" ]; then
+      ln -s "$1" "$2"
+    fi
   fi
 }
 
@@ -18,11 +17,14 @@ for VERSION in $PYTHON_VERSIONS;do
   tar zxf Python-${VERSION}.tgz
   cd /tmp/Python-${VERSION}
   mkdir -p /opt/python-${MAJOR_VERSION}
-  ./configure --prefix=/opt/python-${MAJOR_VERSION} --cache-file=/opt/python-${MAJOR_VERSION}/config.cache --enable-optimizations > /dev/null
+  # --enable-optimizations
+  ./configure --prefix=/opt/python-${MAJOR_VERSION} --cache-file=/opt/python-${MAJOR_VERSION}/config.cache > /dev/null
   make -s -j8
   make altinstall
     
   create_symlink "/opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION}" "/usr/local/bin/python${MAJOR_VERSION}"
+  create_symlink "/opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION}" "/usr/local/bin/python${VERSION}"
   create_symlink "/opt/python-${MAJOR_VERSION}/bin/pip${MAJOR_VERSION}" "/usr/local/bin/pip${MAJOR_VERSION}"
+  create_symlink "/opt/python-${MAJOR_VERSION}/bin/pip${MAJOR_VERSION}" "/usr/local/bin/pip${VERSION}"
 
 done
