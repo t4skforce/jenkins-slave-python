47060955ae8f07fbadb67d88718ef95e9c2b8202
t4skforce <t4skforce1337@gmail.com>
2019-02-24 12:53:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ for VERSION in $PYTHON_VERSIONS;do
   MAJOR_VERSION="${VERSION::3}"
   echo $VERSION $MAJOR_VERSION
   cd /tmp/
-  curl -Ln https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output Python-${VERSION}.tgz
-  tar zxf Python-${VERSION}.tgz
+  curl -Ln https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output Python-${VERSION}.tgz > /dev/null
+  tar zxf Python-${VERSION}.tgz > /dev/null
   cd /tmp/Python-${VERSION}
   mkdir -p /opt/python-${MAJOR_VERSION}
   # --enable-optimizations -> can't enable for all as takes to long for dockerhub
   ./configure --prefix=/opt/python-${MAJOR_VERSION} --cache-file=/opt/python-${MAJOR_VERSION}/config.cache > /dev/null
-  make -s -j8
-  make altinstall
+  make -s -j8 > /dev/null
+  make altinstall > /dev/null
     
   create_symlink "/opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION}" "/usr/local/bin/python${MAJOR_VERSION}"
   create_symlink "/opt/python-${MAJOR_VERSION}/bin/python${MAJOR_VERSION}" "/usr/local/bin/python${VERSION}"
